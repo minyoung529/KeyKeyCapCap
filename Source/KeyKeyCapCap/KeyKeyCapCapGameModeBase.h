91604b006eaf271150c78e8d2eb120847d36c8df
minyoung529 <dlalsdud102613@gmail.com>
2023-08-21 16:11:46 +0900

    Create Empty UE Project

---
+++ Source/KeyKeyCapCap/KeyKeyCapCapGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KeyKeyCapCapGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KEYKEYCAPCAP_API AKeyKeyCapCapGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
