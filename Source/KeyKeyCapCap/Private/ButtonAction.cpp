25637dfaa5e055f49b15d109b9622095ebd75b66
Namsojeong <nsj050320@gmail.com>
2023-08-28 16:28:37 +0900

    Create ButtonAction

--- Source/KeyKeyCapCap/Private/ButtonAction.cpp
+++ Source/KeyKeyCapCap/Private/ButtonAction.cpp
@@ -3,3 +3,56 @@
 
 #include "ButtonAction.h"
 
+void UButtonAction::StartAction()
+{
+}
+
+void UButtonAction::SetScaleTime(float maxScale, float clickCount)
+{
+	MAX_SCALETIME = maxScale;
+	TotalDamage = MAX_SCALETIME / 2;
+
+	addScale = 1 / clickCount;
+}
+
+void UButtonAction::SetValue(float scale)
+{
+	TotalDamage = scale;
+	if (TotalDamage <= MIN_SCALETIME)
+	{
+		Fail();
+	}
+	else if (TotalDamage >= MAX_SCALETIME)
+	{
+		Success();
+	}
+}
+
+
+void UButtonAction::Success()
+{
+	SetScaleTime(10, 10);
+}
+
+void UButtonAction::Fail()
+{
+	SetScaleTime(10, 10);
+}
+
+// Native Func
+
+// Active 될 때 한번
+void UButtonAction::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	SetScaleTime(10, 10);
+}
+
+// Update
+void UButtonAction::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+
+	if(!isStopped)	SetValue((TotalDamage - InDeltaTime)*speed);
+}
--- Source/KeyKeyCapCap/Public/ButtonAction.h
+++ Source/KeyKeyCapCap/Public/ButtonAction.h
@@ -7,29 +7,38 @@
 #include "ButtonAction.generated.h"
 
 class UProgressBar;
-/**
- * 
- */
 UCLASS(HideDropdown)
+
 class KEYKEYCAPCAP_API UButtonAction : public UUserWidget
 {
 	GENERATED_BODY()
 	
 public:
+	UFUNCTION(BlueprintCallable, Category = "Action")
 	void StartAction();
+
+	void SetScaleTime(float maxScale, float clickCount);
+	UFUNCTION(BlueprintCallable, Category = "Action")
+	void SetValue(float scale);
+
 	void Success();
 	void Fail();
 
+protected:
+	virtual void NativeConstruct();
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime);
+
 public:
-	const float MAX_SCALETIME = 10;
+	float MAX_SCALETIME = 10;
 	const float MIN_SCALETIME = 0;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+	float TotalDamage = 0;
+
 	float scaleTime = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
 	float addScale = 0.25;
+	float speed = 0.2;
 
 	bool isStopped = false;
-
-protected:
-	UPROPERTY(meta=(BindWidget))
-	UProgressBar* ActionBar;
 };
