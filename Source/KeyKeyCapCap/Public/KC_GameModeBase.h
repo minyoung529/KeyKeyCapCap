dde553e996c06bfee640fc2dedec09dc5584fcfb
YeEun Oh <OhSolDev@gmail.com>
2023-09-27 01:41:41 +0900

    change default pawn

---
+++ Source/KeyKeyCapCap/Public/KC_GameModeBase.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KC_GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KEYKEYCAPCAP_API AKC_GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+public:
+	AKC_GameModeBase();
+};
